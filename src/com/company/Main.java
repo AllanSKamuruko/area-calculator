4099b96e3449f5016b7215e37d1790c3a58232a7
AllanSKamuruko <allankays47@gmail.com>
2020-06-09 11:34:56 +0200

    area calculator without cube

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" Calculating area");
+
+        System.out.println(" option 1 triangle");
+        System.out.println(" option 2 rectangle");
+        System.out.println(" option 3 cube");
+
+        Scanner input = new Scanner(System.in  );
+        int l,w, b,h;
+        int option;
+        option= input.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println(" enter the base");
+                b=input.nextInt();
+                System.out.println(b);
+
+                System.out.println( " Enter the height");
+                h=input.nextInt();
+                System.out.println(h);
+
+                System.out.println(0.5*h*b);
+                break;
+            case 2:
+                System.out.println(" enter the length ");
+                l = input.nextInt();
+                System.out.println(l);
+                System.out.println(" enter the width");
+                w =input.nextInt();
+                System.out.println(w);
+
+                System.out.println(l*w);
+                break;
+            default :
+                System.out.println("invalid");
+        }
+    }}
+
